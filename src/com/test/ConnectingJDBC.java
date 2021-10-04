98c14a996e5e3f5817bd755c6017971c072bcc06
AjithKumar <mailajithkumars@gmail.com>
2021-10-04 10:23:27 +0530

    Initial Commit : Adding simple java code for connecting jdbc

---
+++ src/com/test/ConnectingJDBC.java
@@ -0,0 +1,32 @@
+package com.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConnectingJDBC {
+
+	public static void main(String[] args) throws Exception {
+
+		// jdbc:postgresql://host:port/database
+		String url = "jdbc:postgresql://localhost:5432/dvdrental";
+		String user = "postgres";
+		String password = "Aji@2867";
+
+		String query = "select * from actor";
+
+		Class.forName("org.postgresql.Driver");
+
+		Connection con = DriverManager.getConnection(url, user, password);
+
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		while (rs.next()) {
+			String name = rs.getString("first_name");
+			System.out.println(name);
+		}
+
+	}
+
+}
